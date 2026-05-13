40d7b5bab87375e7027fed91c53e1f3e76a14323
C. Todd Lombardo <1851211+iamctodd@users.noreply.github.com>
2026-05-13 18:02:34 +0000

    updaing with comments for readability

--- sessions/session01/hello.py
+++ sessions/session01/hello.py
@@ -1,5 +1,14 @@
+# This asks user for input and waits
 x = input("Give me a number less than 5 ") 
-z = int(x)
-y = 6
-if (z < y):
-    print("x is less than y")
+
+# Converts the STRING input into an INTEGER
+z = int(x) 
+
+# Assigns integer 6 to y variable
+y = 6 
+
+# Conditional check - is z less than y?
+if (z < y): 
+    # This runs ONLY if value of z is less than y 
+    print("x is less than y") 
+
