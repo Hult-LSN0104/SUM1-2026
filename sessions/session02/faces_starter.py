9367ac8d9f4e1509aa775fbca8f808ec6926ed2b
C. Todd Lombardo <1851211+iamctodd@users.noreply.github.com>
2026-05-18 12:51:10 +0000

    adding starters and updating files

---
+++ sessions/session02/faces_starter.py
@@ -0,0 +1,6 @@
+# This is a blank file for you to use if you wish.
+# Thie assignmet is here: 
+# https://mycourses.hult.edu/courses/3609319/assignments/39632519?module_item_id=91419303
+# 
+
+#  ADD YOUR CODE BELOW
