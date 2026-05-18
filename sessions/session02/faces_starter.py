7289385f6a6b4aeefe50b99b227b23050630ea8f
C. Todd Lombardo <1851211+iamctodd@users.noreply.github.com>
2026-05-18 14:28:23 +0000

    updated starter files

--- sessions/session02/faces_starter.py
+++ sessions/session02/faces_starter.py
@@ -1,5 +1,5 @@
 # This is a blank file for you to use if you wish.
-# Thie assignmet is here: 
+# Thie assignmet details aree here: 
 # https://mycourses.hult.edu/courses/3609319/assignments/39632519?module_item_id=91419303
 # 
 
