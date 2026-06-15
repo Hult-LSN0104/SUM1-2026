ed0a19dd3c2ba452a0885ce5ffb8fb5a93be200d
C. Todd Lombardo <1851211+iamctodd@users.noreply.github.com>
2026-06-15 11:09:12 +0000

    html updates

--- sessions/session09/hello.py
+++ sessions/session09/hello.py
@@ -1,18 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return "<h1>Hello, Class!</h1>"
-   
 
 if __name__ == "__main__":
     app.run(debug=True)
-
-
-
-
-
-
-# return render_template("hello.html") 
